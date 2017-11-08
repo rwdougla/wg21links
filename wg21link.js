116a355d3d2b7fe401980c99e0b820fc14cc3bd9
Robert Douglas <rwdougla@gmail.com>
2017-11-07 22:18:41 -0600

    Updates:
    - Added links for ewg issues
    - forwarding to https://wg21.link
    - added parse for stable names to link to eel.is/c++draft
    - improved regex matching

--- wg21link.js
+++ wg21link.js
@@ -1,5 +1,5 @@
 function replaceIssues() {
-  var re = /([Ll]{1}[eE]?|[Cc])[wW]{1}[gG]{1}[ ]*([0-9]{4})/g;
+  var re = /\b([Ll]{1}[eE]?|[Cc]|[eE])[wW]{1}[gG]{1}[ ]*([0-9]+)\b/g;
   var regs;
 
   var walker = document.createTreeWalker(
@@ -10,7 +10,7 @@ function replaceIssues() {
       if(parentNodeNotParsed && !nodeIsAlreadyLink) {
         var match = document.createElement('A');
         match.appendChild(document.createTextNode(regs[0]));
-        match.href = 'http://wg21.link/' + regs[1] + 'wg' + regs[2];
+        match.href = 'https://wg21.link/' + regs[1] + 'wg' + regs[2];
 
         match.classList.add('wg21_link');
 
@@ -26,7 +26,7 @@ function replaceIssues() {
 }
 
 function replacePapers() {
-  var re = /([PpDd]{1}[0-9]{4}[rR]{1}[0-9]+|[Nn]{1}[0-9]{4})/g;
+  var re = /\b([PpDd]{1}[0-9]{4}([rR]{1}[0-9]+)?\b|\b[Nn]{1}[0-9]{4})\b/g;
   var regs;
 
   var walker = document.createTreeWalker(
@@ -37,7 +37,7 @@ function replacePapers() {
       if(parentNodeNotParsed && !nodeIsAlreadyLink) {
         var match = document.createElement('A');
         match.appendChild(document.createTextNode(regs[0]));
-        match.href = 'http://wg21.link/' + regs[0];
+        match.href = 'https://wg21.link/' + regs[0];
 
         match.classList.add('wg21_link');
 
@@ -52,5 +52,33 @@ function replacePapers() {
   walker.nextNode();
 }
 
+function replaceStableNames() {
+  var re = /\[([a-zA-Z\.0-9]+)\]/g;
+  var regs;
+
+  var walker = document.createTreeWalker(
+  document.body, NodeFilter.SHOW_TEXT, function(node) {
+    if((regs = re.exec(node.textContent))) {
+      var parentNodeNotParsed = !node.parentNode.classList.contains('wg21_link');
+      var nodeIsAlreadyLink = node.parentNode.tagName == 'A' || node.parentNode.tagName == 'a' || node.tagName == 'A' || node.tagName == 'a';
+      if(parentNodeNotParsed && !nodeIsAlreadyLink) {
+        var match = document.createElement('A');
+        match.appendChild(document.createTextNode(regs[0]));
+        match.href = 'http://eel.is/c++draft/' + regs[1];
+
+        match.classList.add('wg21_link');
+
+        var after = node.splitText(regs.index);
+        after.nodeValue = after.nodeValue.substring(regs[0].length);
+        node.parentNode.insertBefore(match, after);
+      }
+    }
+    return NodeFilter.FILTER_SKIP;
+  }, false);
+
+  walker.nextNode();
+}
 replacePapers();
 replaceIssues();
+replaceStableNames();
+
